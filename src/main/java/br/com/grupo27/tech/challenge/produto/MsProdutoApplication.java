f50f53c53a2398ea90b88cc8ff355ee944e2ae71
Rogerio S Gomes <rogsgomes@gmail.com>
2024-10-14 22:49:01 -0300

    Ajustando a arquivo application

--- src/main/java/br/com/grupo27/tech/challenge/produto/MsProdutoApplication.java
+++ src/main/java/br/com/grupo27/tech/challenge/produto/MsProdutoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProdutoApplication {
+public class MsProdutoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ProdutoApplication.class, args);
+        SpringApplication.run(MsProdutoApplication.class, args);
     }
 
 }
